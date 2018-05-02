1f182959d39316ad6267b266c3a7103fd9776229
ningyou1003 <1079813754@qq.com>
2018-05-02 13:11:48 +0800

    first commit

---
+++ 表结构.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50633
+Source Host           : localhost:3306
+Source Database       : dmjy_cms
+
+Target Server Type    : MYSQL
+Target Server Version : 50633
+File Encoding         : 65001
+
+Date: 2017-06-27 17:05:15
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
